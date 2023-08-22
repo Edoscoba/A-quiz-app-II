e0e56541498a4843c2a400735fecc80121b80b74
obi simon <obisimon@email.com>
2023-08-22 02:10:02 +0100

    questions updated

--- script.js
+++ script.js
@@ -22,28 +22,268 @@ alert("please rate my code");
 	  const quizArray = [
 		{
 		  id: "0",
-		  question: "For every one person, there are 1.6 million ____ ",
-		  options: ["Websites", "Grain Of Sands", "Ants", "Rodents"],
-		  correct: "Ants",
+		  question: "which of the following is NOT correct about javascript variables",
+		  options: ["var Keys can be used to declare a variable ", "Variables are used to store values", "variables must be case sensitive unique ", "Variables can only store strings, numbers and booleans"],
+		  correct: "Variables can only store strings, numbers and booleans",
 		},
 		{
-		  id: "1",
-		  question:
-			"Which is the only continent in the world without a desert?",
-		  options: ["North America", "Asia", "Africa", "Europe"],
-		  correct: "Europe",
-		},
+			id: "1",
+			question: "inside where do we put javascript",
+			options: [
+			  "scripter",
+			  "javascript ",
+			  "scripting",
+			  "script",
+			],
+			correct: "script",
+		  },
+		
 		{
 		  id: "2",
-		  question: " Who invented Computer?",
+		  question: "Where is the correct place to insert a javascript?",
 		  options: [
-			"Charles Babbage",
-			"Henry Luce",
-			"Henry Babbage",
-			"Charles Luce",
+			"both the head and the body section are correct",
+			"none of the above ",
+			"the  body section",
+			"the  head section",
 		  ],
-		  correct: "Charles Babbage",
+		  correct: "both the head and the body section are correct",
 		},
+		{
+		  id: "3",
+		  question: "the external javascript must contain the script tag?",
+		  options: [
+			"true",
+			"false",
+			"",
+			"",
+		  ],
+		  correct: "true",
+		},
+		{
+			id: "o",
+			question: "how do we create a function in javaScript",
+			options: [
+			  "function = myFunction()",
+			  "function  myFunction()",
+			  "function = myFunction()",
+			  "functionMyFunction()",
+			 
+			
+			],
+			correct: "function = myFunction()",
+		  },
+		  {
+			id: "1",
+			question: "which of the following will evaluate as true ",
+			options: [
+			  "'11' === 11",
+			  "true && false",
+			  "!(true || false)",
+			  "'7' == 7",
+			],
+			correct: "'7' == 7",
+		  },
+		  {
+			id: "2",
+			question: "which componenet of for loop run afer each iteration?",
+			options: [
+			  "condition",
+			  "none of the above ",
+			  "initializer",
+			  "final expression",
+			],
+			correct: "final expression",
+		  },
+		  {
+			id: "3",
+			question: "which character is used to make a single comment in javascrpt?",
+			options: [
+			  "#",
+			  "// ",
+			  "||",
+			  "--",
+			],
+			correct: "//",
+		  },
+		  {
+			id: "0",
+			question: "which of the following is not a build in method of string?",
+			options: [
+			  ".sprit()",
+			  ".fixed(n)",
+			  ".indexOf()",
+			  ".toLowerCase()",
+			],
+			correct: ".fixed(n)",
+		  },
+		  {
+			id: "1",
+			question: "which of the following is an example of explicit conversation",
+			options: [
+			  "'$' + 10",
+			  "number('11')",
+			  "Number('11')",
+			  "final expression",
+			],
+			correct:"number('11')",
+		  },
+	
+		  {
+			id: "2",
+			question: "what is javascript",
+			options: [
+			  " A markup language",
+			  " A programming language",
+			  "A styling language",
+			  "A database language",
+			],
+			correct:"A programming language",
+		  },
+		  {
+		  id: "3",
+			question: " Which keyword is used to declare a variable in JavaScript?",
+			options: [
+			  "  const",
+			  "  int",
+			  " char",
+			  "string",
+			],
+			correct:"const",
+		  },
+		  {
+			id: "3",
+			  question: "How do you declare a function in JavaScript?",
+			  options: [
+				"function myFunction()",
+				"myFunction()",
+				"  function = myFunction()",
+				" let myFunction()",
+			  ],
+			  correct:"function myFunction()",
+			},
+			{
+				id: "3",
+				  question: " What is the correct syntax for an if statement in JavaScript?",
+				  options: [
+					" if x = 5",
+					"if(x === 5)",
+					"   if x == 5",
+					"  if x = 5 then",
+				  ],
+				  correct:"if(x === 5)",
+				},
+					{
+				id: "3",
+				  question: "  How do you access the length of an array in JavaScript?",
+				  options: [
+					"length()",
+					"length",
+					"size()",
+					"size",
+					
+				  ],
+				  correct:"length",
+				},
+				{
+					id: "3",
+					  question: " What is the correct way to add an element at the end of an array in JavaScript?",
+					  options: [
+						"arr.push(element)",
+						"  arr.add(element)",
+						" arr.concat(element)",
+						" arr.append(element)",
+						
+					  ],
+					  correct:"arr.push(element)",
+					},
+					{
+					id: "3",
+					  question: "Which operator is used to concatenate strings in JavaScript?",
+					  options: [
+						"+",
+						"&",
+						":",
+						",",
+						
+					  ],
+					  correct:"+",
+					},
+					{
+						id: "3",
+						  question: " What is the output of the following code? \n javascrip console.log('5' + 2);",
+						  options: [
+							"52",
+							"7",
+							"6",
+							"2",
+							
+						  ],
+						  correct:"52",
+						},
+						{
+						id: "3",
+						  question: "Which function is used to convert a string to lowercase in JavaScript?",
+						  options: [
+							" lowercase()",
+							"toLowerCase()",
+							"lowerCase()",
+							" convertToLower()",
+							
+						  ],
+						  correct:"toLowerCase()",
+						},
+						{
+						id: "3",
+						  question: "How do you comment a single line in JavaScript",
+						  options: [
+							" <!--This is a comment-->",
+							"//This is a comment",
+							"/This is a comment/",
+							" %This is a comment%",
+							
+						  ],
+						  correct:"//This is a comment",
+						},
+						{
+							id: "3",
+						
+							  question: " What is the output of the following code? \n javascript \n console.log(typeof undefined",
+							  options: [
+								"undefined",
+								" null",
+								"object",
+								"undefined",
+								
+							  ],
+							  correct:"undefined",
+							},
+
+							{
+							id: "3",
+						
+							  question: " How do you check if a variable is an array in JavaScript?",
+							  options: [
+								"isArray(variable)",
+								"typeofArray(variable)",
+								"Array.isArray(variable)",
+								"undefined",
+								
+							  ],
+							  correct:"Array.isArray(variable)",
+							},
+	
+	
+	
+
+	
+	
+
+
+
+		  
+		  
+		
 		
 	  ];
 	  // restart game
